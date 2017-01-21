b3be172bc95b575af0e7c5685cbcaef5e5b016fb
James W Moody <jamesmoodyradio@gmail.com>
2017-01-21 10:08:45 -0600

    change root to welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-  root 'articles#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
