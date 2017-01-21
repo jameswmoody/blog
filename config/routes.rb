383053bc489e7ed1a7532f6d1686345745ac7621
James W Moody <jamesmoodyradio@gmail.com>
2017-01-21 09:17:19 -0600

    edit gemfile to specify ruby version and set root to articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-  root 'welcome#index'
+  root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
